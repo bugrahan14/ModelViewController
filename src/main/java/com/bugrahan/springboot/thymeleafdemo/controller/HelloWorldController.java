a5e10436761d19b614100268f717a7922356eb6c
bugrahan14 <ertasbugra0611@gmail.com>
2025-10-21 16:09:38 +0300

    NEW UPDATE

--- src/main/java/com/bugrahan/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/bugrahan/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,13 +1,15 @@
 package com.bugrahan.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
     // need a controller method to show initial HTML form
-
     @RequestMapping("/showForm")
     public String showForm(){
 
@@ -21,4 +23,58 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request , Model model){
+
+        // READ THE RREQUEST PARAMETER FROM THE HTML FORM
+        String theName = request.getParameter("studentName");
+
+        // CONVERT THE DATA TO ALL CAPS
+        theName = theName.toUpperCase();
+
+        // CREATE THE MESSAGE
+        String result = "HEY !!" + theName;
+
+        // ADD THE MESSAGE TO THE MODEL
+        model.addAttribute("message" , result);
+
+        return "helloworld";
+
+
+    }
+
+
+    // @RequestParam("studentName") String theName
+    @RequestMapping("/processFormVersionThree")
+    public String letsShoutDudeVersionThree(@RequestParam String studentName, Model model){
+
+
+        // CONVERT THE DATA TO ALL CAPS
+        studentName = studentName.toUpperCase();
+
+        // CREATE THE MESSAGE
+        String result = "HEY VERSİON 3 REQUEST PARAM !!" + studentName;
+
+        // ADD THE MESSAGE TO THE MODEL
+        model.addAttribute("message" , result);
+
+        return "helloworld";
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
